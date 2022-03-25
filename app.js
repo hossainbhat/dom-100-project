36c3573251d164f769897948942aa4c0af4bd586
Md.Hossain Bhat <40881808+hossainbhat@users.noreply.github.com>
2022-03-25 07:43:14 +0600

    project 2 generate hexcode color

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ window.onload = () => {
 function main(){
     const root = document.getElementById('root');
     const btn = document.getElementById('change-btn');
+    const output = document.getElementById('output');
 
     btn.addEventListener('click',function(){
-        const rgbColor = generateRGBColor();
+        const hexColor = generateHexCodeColor();
 
-        root.style.backgroundColor = rgbColor;
+        root.style.backgroundColor = hexColor;
+        output.value = hexColor;
     })
 }
 
-function generateRGBColor(){
+function generateHexCodeColor(){
     const red = Math.floor(Math.random()*255);
     const green = Math.floor(Math.random()*255);
     const blue = Math.floor(Math.random()*255);
 
-    return `rgb(${red},${green},${blue})`
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
 }
